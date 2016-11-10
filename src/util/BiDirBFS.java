c6e0175ccc9e83367a6131430c106417c9b54936
JeremyChen <jeremychen0127@gmail.com>
2016-11-10 15:24:20 -0500

    commit

---
+++ src/util/BiDirBFS.java
@@ -0,0 +1,122 @@
+package util;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Random;
+import java.util.TreeMap;
+
+public class BiDirBFS {
+  public static void main(String[] args) throws NumberFormatException, IOException {
+    String graphFile = args[0];
+    int numHighDegreeBFSTrees = Integer.parseInt(args[1]);
+    int numRandomBFSTrees = Integer.parseInt(args[2]);
+    int numBFSTrees = numHighDegreeBFSTrees + numRandomBFSTrees;
+    int numTrials = Integer.parseInt(args[3]);
+    boolean isDirectedGraph = Boolean.parseBoolean(args[4]);
+    System.out.println("graphFile:" + graphFile);
+    System.out.println("numHighDegreeBFSTrees:" + numHighDegreeBFSTrees);
+    System.out.println("numRandomBFSTrees:" + numRandomBFSTrees);
+    System.out.println("numTrials: " + numTrials);
+    System.out.println("directed: " + isDirectedGraph);
+    long startTime = System.currentTimeMillis();
+    int[][] graph = Utils.getGraph(graphFile);
+    int[][] revGraph = new int[graph.length][];
+    if (isDirectedGraph) {
+      revGraph = Utils.reverseGraph(graph);
+    }
+    long endTime = System.currentTimeMillis();
+    System.out.println("TIME TAKEN TO PARSE THE GRAPH: " + ((endTime - startTime)/1000));
+    Pair.PairComparator degreeComparator = new Pair.PairComparator();
+    Pair[] idDegrees = new Pair[graph.length];
+    for (int i = 0; i < graph.length; ++i) {
+      idDegrees[i] = new Pair(i, graph[i].length);
+    }
+    startTime = System.currentTimeMillis();
+    Arrays.sort(idDegrees, degreeComparator);
+    endTime = System.currentTimeMillis();
+    System.out.println("TIME TAKEN TO SORT DEGREES: " + ((endTime - startTime)/1000));
+
+    SimpleBFSData[] bfsTrees = new SimpleBFSData[numHighDegreeBFSTrees + numRandomBFSTrees];
+    SimpleBFSData[] revBfsTrees = new SimpleBFSData[numHighDegreeBFSTrees + numRandomBFSTrees];
+    startTime = System.currentTimeMillis();
+    for (int i = 0; i < numHighDegreeBFSTrees; ++i) {
+      System.out.println("Source of High Degree BFS Tree vertex: " + idDegrees[idDegrees.length - (i + 1)].id);
+      bfsTrees[i] = BFSImplementations.getBFSTree(graph, idDegrees[idDegrees.length - (i + 1)].id);
+      if (isDirectedGraph) {
+        revBfsTrees[i] = BFSImplementations.getBFSTree(revGraph, idDegrees[idDegrees.length - (i + 1)].id);
+      }
+    }
+
+    Random random = new Random(0);
+    for (int i = 0; i < numRandomBFSTrees; ++i) {
+      int nextSrc = random.nextInt(graph.length);
+      if (graph[nextSrc].length == 0) {
+        i--;
+        continue;
+      } else {
+        System.out.println("Source of Random BFS Tree vertex: " + nextSrc);
+        bfsTrees[numHighDegreeBFSTrees + i] = BFSImplementations.getBFSTree(graph, nextSrc);
+        if (isDirectedGraph) {
+          revBfsTrees[numHighDegreeBFSTrees + i] = BFSImplementations.getBFSTree(revGraph, nextSrc);
+        }
+      }
+    }
+    endTime = System.currentTimeMillis();
+    int totalNumBFSTrees = numHighDegreeBFSTrees + numRandomBFSTrees;
+    System.out.println("TIME TAKEN TO CONSTRUCT" + totalNumBFSTrees
+      + " BFS TREES: " + ((endTime - startTime)/1000));
+    System.out.println("AVG TIME TAKEN TO CONSTRUCT 1 BFS TREE: "
+      + ((endTime - startTime)/(1000*totalNumBFSTrees)));
+
+    // parentHistogram.get(vertexId).get(neighborId) is the count
+    ArrayList<TreeMap<Integer, Integer>> parentHistogram = Utils.getParentHistogram(graph, bfsTrees);
+
+    // print # of BFS trees that each neighbor to be the vertex's parent
+    for (int v = 0; v < parentHistogram.size(); ++v) {
+//      System.out.println("===== Vertex " + v + "=====");
+      for(Map.Entry<Integer,Integer> entry : parentHistogram.get(v).entrySet()) {
+        Integer neighbor = entry.getKey();
+        Integer numParents = entry.getValue();
+
+//        System.out.println("Neighbor: " + neighbor + ", # of trees to be the parent: " + numParents);
+      }
+    }
+
+    int src, dst;
+    long numEdgesShortestPath;
+    int shortestPathLength;
+    int numAbleToFindPath = 0;
+    long numEdgesShortestPathSum = 0;
+    long totalTimeForBiDirSSSDSP = 0;
+
+    for (int i = 0; i < numTrials; ++i) {
+      if (i > 0 && (i % 1000) == 0) {
+        System.out.println("Starting " + i + "th trial.");
+      }
+
+      // Generates random queries from s to t with s != t
+      src = random.nextInt(graph.length);
+      dst = random.nextInt(graph.length);
+      while (src == dst) {
+        dst = random.nextInt(graph.length);
+      }
+
+      startTime = System.nanoTime();
+      shortestPathLength = Utils.getSSSDSPBiDirBFS(graph, src, dst);
+      endTime = System.nanoTime();
+      totalTimeForBiDirSSSDSP += (endTime - startTime);
+      numEdgesShortestPath = Utils.numEdgesTraversed;
+
+      if (shortestPathLength > 0) {
+        numEdgesShortestPathSum += numEdgesShortestPath;
+        numAbleToFindPath++;
+      }
+    }
+
+    System.out.println("totalTimeForBiDirSSSDSP: " + totalTimeForBiDirSSSDSP);
+    System.out.println("Avg #Edges Traversed of BFS: " + (1.0 * numEdgesShortestPathSum / numAbleToFindPath));
+  }
+}
